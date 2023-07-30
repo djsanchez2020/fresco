fcb04e3bd1c739fa87fe3e356e9a98b127312572
Admin <admin.push@hackerrank.workspace>
2023-07-30 15:00:24 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- angular/src/app/dashboard/holiday-editor/holiday-editor.component.ts
+++ angular/src/app/dashboard/holiday-editor/holiday-editor.component.ts
@@ -11,9 +11,9 @@ export class HolidayEditorComponent implements OnInit, OnChanges {
   // get the city from dashboard
   @Input() city = '';
 
-  // get the selected date from hoilday view and assigin it to selectedDate
+  // get the selected date from holiday view and assign it to selectedDate
   selectedDate = '';
-  // get the respose and assign it to holidayObj
+  // get the response and assign it to holidayObj
   holidayObj: any = {};
 
   // Use the editorFlag to display or hide form
@@ -30,66 +30,115 @@ export class HolidayEditorComponent implements OnInit, OnChanges {
   }
 
   /**
-   * When city change get Holiday information
+   * When city changes, get Holiday information
    */
   ngOnChanges(changes: SimpleChanges): void {
-
+    if (changes.city && !changes.city.firstChange) {
+      this.getSelectedHolidayInfo();
+    }
   }
 
   /**
-   * Get user selected date from holiday view component and assign it to selectedDate
-   * Call getSelectedHolidayInfo() when user selects date
+   * Get user-selected date from holiday view component and assign it to selectedDate
+   * Call getSelectedHolidayInfo() when the user selects a date
    */
   ngOnInit() {
-
+    this.holidayEditor = new FormGroup({
+      holidayName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')])
+    });
   }
 
   /**
    *    ---> getSelectedHolidayInfo()<---
    *
-   * Validate selected date with current date.
-   *  ->  If selected date is greater then do the following
-   *      1. Display editor
-   *      2. Get Holiday information and assign it to holidayObj variable.
-   *  -> If selected date is lesser then do the following.
-   *      1. Hide editor
+   * Validate the selected date with the current date.
+   *  ->  If the selected date is greater than the current date, do the following
+   *      1. Display the editor
+   *      2. Get Holiday information and assign it to the holidayObj variable.
+   *  -> If the selected date is lesser than the current date, do the following.
+   *      1. Hide the editor
    *      2. Do not fetch holiday information
    */
   getSelectedHolidayInfo() {
-  
+    const currentDate = new Date();
+    const selectedDateObj = new Date(this.selectedDate);
+    if (selectedDateObj > currentDate) {
+      this.editorFlag = true;
+      this.holidayServiceObj.getSelectedHolidayInfo(this.selectedDate, this.city).subscribe(
+        (response) => {
+          this.holidayObj = response;
+          this.holidayEditor.patchValue({
+            holidayName: this.holidayObj?.holidayName || ''
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      this.editorFlag = false;
+      this.holidayObj = {};
+    }
   }
 
   /**
    *    ---> addHoliday()<---
    * Add Holiday
-   * After adding holiday implement the following scenario:
-   *    -> Notify holiday view component
+   * After adding a holiday, implement the following scenario:
+   *    -> Notify the holiday view component
    *    -> Get Holiday information
    */
   addHoliday() {
-
+    const holidayName = this.holidayEditor.get('holidayName').value;
+    this.holidayServiceObj.addHoliday(this.selectedDate, this.city, holidayName).subscribe(
+      (response) => {
+        this.holidayEditor.reset();
+        this.getSelectedHolidayInfo();
+        this.holidayServiceObj.monthComponentNotify();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   /**
    *    ---> updateHoliday()<---
    * Update Holiday
-   * After updating holiday implement the following scenario:
-   *    -> Notify holiday view component
+   * After updating a holiday, implement the following scenario:
+   *    -> Notify the holiday view component
    *    -> Get Holiday information
    */
   updateHoliday() {
-
+    const holidayName = this.holidayEditor.get('holidayName').value;
+    this.holidayServiceObj.updateHoliday(this.holidayObj.id, this.selectedDate, this.city, holidayName).subscribe(
+      (response) => {
+        this.getSelectedHolidayInfo();
+        this.holidayServiceObj.monthComponentNotify();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   /**
    *    ---> removeHoliday()<---
    * Remove Holiday
-   * After removing holiday implement the following scenario:
-   *    -> Notify holiday view component
-   *    -> User should be able to add new Holiday
+   * After removing a holiday, implement the following scenario:
+   *    -> Notify the holiday view component
+   *    -> User should be able to add a new Holiday
    */
   removeHoliday() {
-   
+    this.holidayServiceObj.removeHoliday(this.holidayObj.id).subscribe(
+      (response) => {
+        this.holidayEditor.reset();
+        this.getSelectedHolidayInfo();
+        this.holidayServiceObj.monthComponentNotify();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
